85bccd2fdbf8f86abf5a2e0fb3fbce6614f5fd94
ErikaArci99 <erikaarci1999@outlook.it>
2025-09-24 14:46:24 +0200

    Add pizza editing functionality; implement edit form, update methods, and create edit template

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ public class PizzaController {
     @Autowired
     private PizzaRepository pizzaRepository;
 
+    // mostra tutte le pizze
     @GetMapping("/")
     public String index(Model model) {
         List<Pizza> pizze = pizzaRepository.findAll();
@@ -28,6 +30,7 @@ public class PizzaController {
         return "index";
     }
 
+    // mosra la pzza con id:
     @GetMapping("/pizza/{id}")
     public String showPizza(@PathVariable("id") Integer id, Model model) {
         Pizza pizza = pizzaRepository.findById(id.longValue()).orElse(null);
@@ -58,5 +61,30 @@ public class PizzaController {
         }
         pizzaRepository.save(pizza);
         return "redirect:/";
+    }
+
+    // Modifica pizza - mostra form
+    @GetMapping("/pizza/edit/{id}")
+    public String edit(@PathVariable Long id, Model model) {
+        Pizza pizza = pizzaRepository.findById(id).orElse(null);
+        if (pizza == null) {
+            return "redirect:/"; // se non trova la pizza torna alla lista
+        }
+        model.addAttribute("pizza", pizza);
+        return "edit";
+    }
+
+    // UPDATE - salvataggio modifica pizza
+    @PostMapping("/pizza/edit/{id}")
+    public String update(@PathVariable Long id,
+            @Valid @ModelAttribute("pizza") Pizza formPizza,
+            BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            return "edit";
+        }
+        formPizza.setId(id); // assicura che stiamo aggiornando e non creando
+        pizzaRepository.save(formPizza);
+        return "redirect:/pizza/" + id; // dopo update rimanda alla show
     }
 }
